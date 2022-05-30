341225d5c2138bf330eb1bc1846c30b290152cfd
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-05-30 13:54:12 +0900

    feat: 날짜 기능 추가

--- src/routes/users/userDetail/index.tsx
+++ src/routes/users/userDetail/index.tsx
@@ -1,20 +1,15 @@
 import styles from './userDetail.module.scss'
-import 'react-datepicker/dist/react-datepicker.css'
-import ReactDatePicker from 'react-datepicker'
-import { useState } from 'react'
 import { useAppDispatch } from 'hooks/useAppDispatch'
 import { useAppSelector } from 'hooks/useAppSelector'
 import { useParams } from 'react-router-dom'
 import { useMount } from 'react-use'
 import { getHeartRateApi } from 'services/getData'
 import { temp, tempData } from 'states/heartRateData'
+import DatePicker from 'components/userDetail/DatePicker'
 
 const title = ['로그인', '회원번호', '가입일시']
 
 const UserDetail = () => {
-  const [startDate, setStartDate] = useState(new Date())
-  const [endDate, setEndDate] = useState(new Date())
-  const minDate = new Date()
   const dispatch = useAppDispatch()
 
   useMount(() => {
@@ -45,34 +40,9 @@ const UserDetail = () => {
           <div className={styles.chart}>차트들어갈자리</div>
           <span>날짜</span>
           <span>bpm/걸음</span>
-          <div className={styles.chartWrap}>
-            <div className={styles.lookUp}>조회기간</div>
-            <ReactDatePicker
-              className={styles.startDate}
-              minDate={minDate}
-              dateFormat='전체'
-              selected={startDate}
-              onChange={(date: Date) => setStartDate(date)}
-              startDate={startDate}
-              endDate={endDate}
-            />
-            <p className={styles.p}>~</p>
-            <ReactDatePicker
-              className={styles.startDate}
-              selected={endDate}
-              onChange={(date: Date) => setEndDate(date)}
-              dateFormat='전체'
-              startDate={startDate}
-              endDate={endDate}
-              minDate={startDate}
-              selectsEnd
-            />
-          </div>
-          <button type='button'>오늘</button>
-          <button type='button'>일주일</button>
-          <button type='button'>전체</button>
         </div>
       </div>
+      <DatePicker />
     </div>
   )
 }
