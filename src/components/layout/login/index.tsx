40116b3bb55161a62916c69296a8126869a16d7f
hamkke <walwalzz@naver.com>
2022-05-30 21:00:10 +0900

    임의 아이디 추가, 경고창 기능 구현

--- src/components/layout/login/index.tsx
+++ src/components/layout/login/index.tsx
@@ -8,25 +8,41 @@ import { CheckIcon } from 'assets/svgs'
 import LOGO from 'assets/logo-w.png'
 import { accessActions, getAccessState } from 'states/accessUser'
 
-const TEMPORARY = {
-  id: 'qwe123',
-}
+const TEMPORARY = [
+  {
+    id: 'qwe123',
+  },
+  {
+    id: 'asd123',
+  },
+  {
+    id: 'zxc123',
+  },
+]
 const Login = () => {
   const dispatch = useAppDispatch()
   // const userState = useAppSelector(getAccessState)
   const [checkId, setCheckId] = useState(false)
   const [showTxt, setShowTxt] = useState(false)
+  const [showPopup, setShowPopup] = useState(false)
 
   const navigate = useNavigate()
 
   const handleCheckId = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.currentTarget.value
     setShowTxt(true)
+    setShowPopup(true)
     setCheckId(false)
-    if (TEMPORARY.id === target) {
-      setShowTxt(false)
-      setCheckId(true)
-    } else if (target === '') {
+    TEMPORARY.filter((a) => {
+      if (a.id === target) {
+        setShowTxt(false)
+        setShowPopup(false)
+        setCheckId(true)
+      }
+      return false
+    })
+    if (target === '') {
+      setShowPopup(false)
       setShowTxt(false)
     }
   }
@@ -35,10 +51,17 @@ const Login = () => {
     dispatch(accessActions.setAccessUser(checkId))
     navigate('/home')
   }
-
+  const handlePopup = () => {
+    setShowPopup(false)
+  }
   return (
     <div className={styles.loginWrap}>
-      <div className={styles.popup}>일치하는 아이디가 없습니다</div>
+      <div className={`${styles.popup} ${showPopup ? styles.popupOK : ''}`}>
+        일치하는 아이디가 없습니다
+        <button type='button' onClick={handlePopup}>
+          X
+        </button>
+      </div>
       <div className={styles.formWrap}>
         <div className={styles.loginForm}>
           <img src={LOGO} alt='모아테이타 로고' />
